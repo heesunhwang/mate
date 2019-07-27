58d867fe3b10456ff3f987a04cf3487127a8f6fc
seenewmax <donaskwhy@nate.com>
2019-07-27 17:16:06 +0900

    removed scaffold Post

--- app/models/post.rb
+++
@@ -1,7 +0,0 @@
-class Post < ApplicationRecord
-    after_commit :notify_pusher, on: [:create, :update]
-    
-    def notify_pusher
-        Pusher.trigger('post', 'new', self.as_json)
-    end
-end
