d2d69424c0c150c4bd40615a16f08051c6ce8ee3
seenewmax <donaskwhy@nate.com>
2019-08-06 03:45:03 +0900

    review authenticate debug

--- app/controllers/picnics_controller.rb
+++ app/controllers/picnics_controller.rb
@@ -1,7 +1,14 @@
 class PicnicsController < ApplicationController
   def index
+    if params[:search]
+      @search_result = Item.where('title || content LIKE ?', "%#{params[:search]}%")
+      byebug
+    else
+      @search_result = Item.all
+    end
     @areas = Item.areas.keys
     @categories = Item.categories.keys
   end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-  get 'reviews/new'
   root 'home#index'
 
   resources :diaries
